2acb6f49a100b17181ff49921242b3b18988d200
james <jamesanton123@gmail.com>
2017-11-07 18:32:17 -0500

    added some stuff

--- src/main/java/learning/WekaDemo.java
+++ src/main/java/learning/WekaDemo.java
@@ -14,12 +14,22 @@ import weka.classifiers.rules.PART;
 import weka.classifiers.trees.DecisionStump;
 import weka.classifiers.trees.J48;
 import weka.core.FastVector;
+import weka.core.Instance;
 import weka.core.Instances;
 
 /**
  * Tutorial based on:
  * http://www.programcreek.com/2013/01/a-simple-machine-learning-example-in-
  * java/
+ * 
+ * In this example, weka will attempt to predict the weather, based on the attributes given.
+ * The use case here, is you have some data, and you want to classify it.
+ * sunny = 0, overcast = 1, rainy = 2
+ * 
+ * 
+ * 
+ * 
+ * 
  */
 public class WekaDemo {
 	private Classifier[] models = { 
@@ -30,21 +40,35 @@ public class WekaDemo {
 	};
 
 	public static void main(String[] args) throws Exception {
-		new WekaDemo().runDemo();
+		new WekaDemo().findMostAccurateModel();
 	}
 
-	private void runDemo() throws Exception {
-		Instances data = FileUtil.getData("weather.txt", true);		
+	private void findMostAccurateModel() throws Exception {
+		// Read the data from the file
+		Instances data = FileUtil.getData("weather.txt", true);
+		
+		// Split up the data, some to be used as training data and some as testing data
 		SplitData splitData = SplitterUtil.crossValidationSplit(data, 10);
+		
 		List<Instances> trainingData = splitData.getTrainingData();
-		List<Instances> testingData = splitData.getTestingData();		
+		
+		// For each type of model
 		for (Classifier model: models) {
 			FastVector predictions = new FastVector();
 			for (int i = 0; i < trainingData.size(); i++) {
-				Evaluation validation = ClassificationUtil.classify(model, trainingData.get(i), testingData.get(i));
+				Instances testData = splitData.getTestingData().get(i);
+				
+				// Make a prediction for the test data
+				Evaluation validation = ClassificationUtil.classify(model, trainingData.get(i), testData);
+				
+				// Add it to the list of predictions
 				predictions.appendElements(validation.predictions());
 			}
 			printClassificationAccuracy(model, AccuracyCalculator.calculateAccuracy(predictions));
+			
+			// Just showing here how you could do a prediction
+			double prediction = model.classifyInstance(data.instance(10));
+			System.out.println(prediction + " " + data.instance(10).classValue());
 		}
 	}
 
--- src/main/java/learning/util/ClassificationUtil.java
+++ src/main/java/learning/util/ClassificationUtil.java
@@ -7,10 +7,8 @@ import weka.core.Instances;
 public class ClassificationUtil {
 	public static Evaluation classify(Classifier model, Instances trainingSet, Instances testingSet) throws Exception {
 		Evaluation evaluation = new Evaluation(trainingSet);
-
 		model.buildClassifier(trainingSet);
 		evaluation.evaluateModel(model, testingSet);
-
 		return evaluation;
 	}
 }
